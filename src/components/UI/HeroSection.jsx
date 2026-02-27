e61d2dc2e70fc438b047ab937b96ba8587e2be8d
rohitkrroy <rohitydv9606@gmail.com>
2026-02-28 02:34:09 +0530

    updated

--- src/components/UI/HeroSection.jsx
+++ src/components/UI/HeroSection.jsx
@@ -1,4 +1,5 @@
 import { FaLongArrowAltRight } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 
 export const HeroSection = () => {
   return (
@@ -12,9 +13,10 @@ export const HeroSection = () => {
             Discover the history, culture, and beauty of every nation. Sort,
             search, and filter through countries to find the details you need.
           </p>
-          <button className="btn btn-darken btn-inline bg-white-box">
-            Start Exploring <FaLongArrowAltRight />
-          </button>
+
+          <NavLink to="/about">
+            <button>Start Exploring <FaLongArrowAltRight /></button>
+          </NavLink>
         </div>
         <div className="hero-image">
           <img
