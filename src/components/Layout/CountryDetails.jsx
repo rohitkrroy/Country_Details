ea92e53d1e4bf46553f9ae753c94319b96a6d24a
rohitkrroy <rohitydv9606@gmail.com>
2026-03-17 19:47:00 +0530

    updated title

--- src/components/Layout/CountryDetails.jsx
+++ src/components/Layout/CountryDetails.jsx
@@ -4,7 +4,7 @@ import { getCountryIndData } from "../../api/postApi";
 import { Loader } from "../UI/Loader";
 
 export const CountryDetails = () => {
-  const params = useParams();
+  const params = useParams();  
 
   const [isPending, startTransition] = useTransition();
   const [country, setCountry] = useState();
